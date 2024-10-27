08d0709cfdc9efe053157c4f987309fffeb324b9
jacky55321 <jacky55321261@gmail.com>
2024-10-27 15:29:04 +0800

    修改firebaseAdmin的金鑰環境變數

--- functions/config/firebaseAdmin.js
+++ functions/config/firebaseAdmin.js
@@ -2,11 +2,19 @@ const admin = require("firebase-admin");
 const { initializeApp, cert } = require("firebase-admin/app");
 const { getFirestore, FieldValue } = require("firebase-admin/firestore");
 const { getStorage } = require("firebase-admin/storage");
-const serviceAccount = require("./serviceAccountKey.json");
+
+// 根據環境條件動態加載憑證
+if (process.env.FIREBASE_SERVICE_ACCOUNT_KEY) {
+  // 在生產環境中，從環境變數加載 Service Account 憑證
+  serviceAccountKey = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
+} else {
+  // 在本地開發環境中，從 JSON 文件中加載憑證
+  serviceAccountKey = require("./serviceAccountKey.json");
+}
 
 // 初始化 Firebase Admin SDK
 initializeApp({
-  credential: cert(serviceAccount),
+  credential: cert(serviceAccountKey),
   storageBucket: 'sideproject2405-b8a66.appspot.com'
 });
 
